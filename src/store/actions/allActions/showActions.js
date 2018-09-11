fcbaa266d743c1af34fbc881b6e1709a8c056628
qazz201 <39721136+qazz201@users.noreply.github.com>
2018-09-11 08:57:53 +0300

    Update showActions.js

--- src/store/actions/allActions/showActions.js
+++ src/store/actions/allActions/showActions.js
@@ -20,25 +20,3 @@ export var showsHasError = bool => {
   };
 };
 
-/*export var fetchShowsData = (urlRequest, typeOfRequest) => async dispatch => {
-  dispatch(showsIsLoading(true));
-  // console.log(urlRequest, "URL__ACTION");
-  switch (typeOfRequest) {
-    case "SEARCH":
-      dispatch();
-  }
-
-  try {
-    var showsData = await fetchShows(urlRequest);
-
-    dispatch(showsFetchSuccess(showsData));
-    console.log(showsData, "showsData from SHOWACTIONS");
-  } catch (error) {
-    dispatch(showsHasError(true));
-
-    console.log(`ERROR:${error.message}`);
-    throw new Error(`SOMETHING WRONG:ERROR:${error.message}`);
-  }
-  dispatch(showsIsLoading(false));
-};
-*/
